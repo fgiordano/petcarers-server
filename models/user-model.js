a6d6442df062022cdef1352793438a1d7223745e
Francesca Giordano <francesca020406@hotmail.com>
2017-12-11 18:24:04 -0500

    adds to user schema

--- models/user-model.js
+++ models/user-model.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+  email      : { type: String, required: true },
+  username   : { type: String, required: true },
+  password   : { type: String, required: true },
+  aboutme    : { type: String, required: true },
+  role       : { type: String, enum: ROLES, required: true },
+  pet        : { type: Schema.Types.ObjectId, ref: 'Pet'},
+  image      : { type: String, default: "https://www.menon.no/wp-content/uploads/person-placeholder.jpg" }
 }, {
   timestamps: {
     createdAt: 'created_at',
