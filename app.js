9685a87f7845c135b39ca3585d11bcc47b8d90d5
Francesca Giordano <francesca020406@hotmail.com>
2017-12-11 20:13:02 -0500

    adds session initializer

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+const authRoutes = require('./routes/auth-routes');
+
+require('./config/database-setup');
+require('./config/passport-setup');
 
 var app = express();
 
@@ -21,9 +27,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	session({
+		secret: 'string is to avoid a warning',
+		resave: true,
+		saveUninitialized: true
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/', authRoutes);
+
+app.use((req, res, next) => {
+  res.sendfile(__dirname + '/public/index.html');
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
