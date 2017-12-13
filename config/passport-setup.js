b33f39b827c705f7e80bece94f499ce76efa0b4c
Francesca Giordano <francesca020406@hotmail.com>
2017-12-13 17:02:34 -0500

    fixes passpot setup

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,43 +1,23 @@
-const LocalStrategy = require('passport-local').Strategy;
-const User          = require('../models/user-model');
-const bcrypt        = require('bcrypt');
+const passport = require('passport');
 
-module.exports = function (passport) {
+const User = require('../models/user-model');
 
-  passport.use(new LocalStrategy((username, password, next) => {
-    User.findOne({ username }, (err, foundUser) => {
-      if (err) {
-        next(err);
-        return;
-      }
+//serialize: save only the Id of the user document in the session
+passport.serializeUser((loggedInUser, cb) => {
+  cb(null, loggedInUser._id);
 
-      if (!foundUser) {
-        next(null, false, { message: 'Incorrect username' });
-        return;
-      }
+});
 
-      if (!bcrypt.compareSync(password, foundUser.password)) {
-        next(null, false, { message: 'Incorrect password' });
-        return;
-      }
+// deserializa: retrive the full user details from the database using the Id 
+//(the user i stored in the session)
+passport.deserializeUser((userIdFromSession, cb) => {
+  User.findById(userIdFromSession, (err, userDocument) => {
+    if (err) {
+      cb(err);
+      return;
+    }
 
-      next(null, foundUser);
-    });
-  }));
+    cb(null, userDocument);
+  })
 
-  passport.serializeUser((loggedInUser, cb) => {
-    cb(null, loggedInUser._id);
-  });
-
-  passport.deserializeUser((userIdFromSession, cb) => {
-    User.findById(userIdFromSession, (err, userDocument) => {
-      if (err) {
-        cb(err);
-        return;
-      }
-
-      cb(null, userDocument);
-    });
-  });
-
-}
+});
