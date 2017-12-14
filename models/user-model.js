cb5b5a6872083e55e0f45168ea55dde8123061e0
Francesca Giordano <francesca020406@hotmail.com>
2017-12-13 20:48:01 -0500

    installs passport local

--- models/user-model.js
+++ models/user-model.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  email      : { type: String, required: true },
+  email      : { type: String},
   username   : { type: String, required: true },
   password   : { type: String, required: true },
-  aboutme    : { type: String, required: true },
-  role       : { type: String, enum: ROLES, required: true },
-  pet        : { type: Schema.Types.ObjectId, ref: 'Pet'},
+  aboutme    : { type: String},
+  // role       : { type: String, enum: ROLES },
+  // pet        : { type: Schema.Types.ObjectId, ref: 'Pet'},
   image      : { type: String, default: "https://www.menon.no/wp-content/uploads/person-placeholder.jpg" }
 }, {
   timestamps: {
