65698262388645c2111e9b146061350c627f593d
Francesca Giordano <francesca020406@hotmail.com>
2017-12-16 12:25:21 -0500

    edits pet model and pet routes

---
+++ routes/pet-routes.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+const Pet = require('../models/pet-model');
+// const upload = require('../configs/multer');
+
+/* GET Pet listing. */
+router.get('/pets', (req, res, next) => {
+  Pet.find((err, phonesList) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+    res.json(petList);
+  });
+});
+
+/* CREATE a new Pet. */
+router.post('/pets', upload.single('file'), function(req, res) {
+  const pet = new Pet({
+    name: req.body.name,
+    age: req.body.age,
+  });
+
+  pet.save((err) => {
+    if (err) {
+      return res.send(err);
+    }
+
+    return res.json({
+      message: 'New Pet added!',
+      pet: pet
+    });
+  });
+});
