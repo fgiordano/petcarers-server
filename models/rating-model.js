4ef1f00509c7f6f3570151b7d678e048ecb59ac8
Francesca Giordano <francesca020406@hotmail.com>
2017-12-16 10:23:41 -0500

    adds mongoose

---
+++ models/rating-model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const reviewSchema = new Schema({
+  content: {
+    type: String,
+    required: [true, 'Please tell us about your review.'],
+    minlength: [50, 'Please write at least 50 characters.'],
+    maxlength: [400, 'Type less please.']
+  },
+  stars: {
+    type: Number,
+    required: [true, 'Rate the product.'],
+    min: [1, 'Ratings can be no lower than 1 star.'],
+    max: [5, 'Ratings can be no better than 5 stars.']
+  },
+  author: {
+    type: String,
+    required: [true, 'Please provide your name']
+  },
+  // BY REFERENCE INSTEAD OF SUBDOCUMENTS
+  // product: { type: Schema.Types.ObjectId }
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+
+module.exports = Review;
