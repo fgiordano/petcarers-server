401849d8dccec25b8ce9f034e75225e0e685ad7c
Francesca Giordano <francesca020406@hotmail.com>
2017-12-16 12:25:51 -0500

    edits pet model and pet routes

--- models/pet-model.js
+++ models/pet-model.js
@@ -1,18 +1,18 @@
 const mongoose  = require('mongoose');
 const Schema    = mongoose.Schema;
 const PETTYPE   = require('./pet-types');
-const User     = require('./user');
+const User     = require('./user-model');
 
 
 const PetSchema = new Schema({
-  owner      : { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  // owner      : { type: Schema.Types.ObjectId, ref: 'User', required: true },
   name       : { type: String, required: true },
-  type       : { type: String, enum: PETTYPE, required: true },
-  breed      : { type: String },
-  aboutme    : { type: String, required: true },
-  weight     : { type: Number, required: true },
+  // type       : { type: String, enum: PETTYPE, required: true },
+  // breed      : { type: String },
+  // aboutme    : { type: String, required: true },
+  // weight     : { type: Number, required: true },
   age        : { type: Number, required: true },
-  image     : { type: String, default: "https://www.menon.no/wp-content/uploads/person-placeholder.jpg" }
+  // image     : { type: String, default: "https://www.menon.no/wp-content/uploads/person-placeholder.jpg" }
   // imgUrl     : { type: String, default: "https://placeholdit.imgix.net/~text?txtsize=33&txt=250%C3%97250&w=250&h=250" }
 });
 
