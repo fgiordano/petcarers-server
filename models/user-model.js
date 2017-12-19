eaaf281cf043b4cb44379b69bd99921aa9c2c0d4
Francesca Giordano <francesca020406@hotmail.com>
2017-12-18 21:17:38 -0500

    adds ratings to user model

--- models/user-model.js
+++ models/user-model.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
-const ROLES     = require('./roles-types');
-const Pet       = mongoose.model.Pet;
+const ROLES    = require('./roles-types');
+const Pet      = mongoose.model.Pet;
+const Rating   = mongoose.model.Rating;
 
 
 const userSchema = new Schema({
-  email      : { type: String},
-  username   : { type: String, required: true },
-  password   : { type: String, required: true },
-  aboutme    : { type: String},
-  // role       : { type: String, enum: ROLES },
-  pets       : [{ type: Schema.Types.ObjectId, 'default': [], ref: 'Pet'}],
-  image      : { type: String, default: "https://www.menon.no/wp-content/uploads/person-placeholder.jpg" }
+  email           : { type: String},
+  username        : { type: String, required: true },
+  password        : { type: String, required: true },
+  aboutme         : { type: String},
+  // role         : { type: String, enum: ROLES },
+  pets            : [{ type: Schema.Types.ObjectId, 'default': [], ref: 'Pet'}],
+  receivedRatings : [{ type: Schema.Types.ObjectId, 'default': [], ref: 'Rating'}],
+  image           : { type: String, default: "https://www.menon.no/wp-content/uploads/person-placeholder.jpg" }
 }, {
   timestamps: {
     createdAt: 'created_at',
